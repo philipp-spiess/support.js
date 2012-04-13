587ddc730c23dc50d384699c19df38f595f2632f
Philipp Spieß <philipp.spiess@myxcode.at>
2012-04-13 18:09:10 +0200

    Heroku support (-.-)

--- web.js
+++ web.js
@@ -3,20 +3,20 @@
  * @author Philipp Spieß <hello@philippspiess.com>
  **/
 
-
 /**
  * The Campfire interactor
  */
 var camp = require('./lib/campfire').connect()
 
-
 /**
  * The webserver
  */
-var app = require('./lib/server').server.listen(process.env.PORT || 3000)
-
+var app = require('./lib/server').server
 
 /**
  * The socket.io realtime part
  */
 var socket = require('./lib/socket.io').connect(app)
+
+
+app.listen(process.env.PORT || 3000)
